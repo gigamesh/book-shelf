4e0f893fcfc9cc53656783cf77e46f3cd66cf356
gigamesh <gigamesh@users.noreply.github.com>
2018-05-06 21:40:06 -0700

    first commit!

---
+++ client/src/containers/home_container.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getBooks } from '../actions';
+import BookItem from '../widgetsUI/book_item';
+
+class HomeContainer extends Component {
+
+  componentWillMount(){
+    this.props.dispatch(getBooks(3,0,'desc'));
+  }
+
+  renderItems = (books) => (
+    books.list ?
+    books.list.map(item => (
+      <BookItem {...item} key={item._id}/>
+    ))
+    : null
+  )
+
+  render() {
+    return (
+      <div>
+        {this.renderItems(this.props.books)}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state){
+  return {
+    books: state.books
+  }
+}
+
+export default connect(mapStateToProps)(HomeContainer);
