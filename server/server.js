eb770111d476505d1055a166314849fd1ca59b0a
gigamesh <gigamesh@users.noreply.github.com>
2018-05-07 18:38:30 -0700

    server prepped for production

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ const { auth } = require('./middleware/auth');
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use(express.static('client/build'))
+
 // GET //
 app.get('/api/auth', auth,(req,res)=>{
   res.json({
@@ -152,6 +154,14 @@ app.delete('/api/delete_book',(req,res)=>{
     res.json(true);
   })
 })
+
+// PRODUCTOIN BUILD SET UP
+if(process.env.NODE_ENV === 'production'){
+  const path = require('path');
+  app.get('/*',(req,res)=>{
+    res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+  })
+}
 
 const port = process.env.PORT || 3001;
 app.listen(port,()=> {
